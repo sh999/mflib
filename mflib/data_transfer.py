aa3acecba8071700836d1a8b8cccefcbdf6ebfc9
jackhancockuky <jcha265@uky.edu>
2023-09-01 16:38:22 -0400

    Made some changes to data_transfer.py and added prometheus import

--- mflib/data_transfer.py
+++ mflib/data_transfer.py
@@ -73,7 +73,7 @@ class ElkViewer:
             print(f"Fail: {e}")
 
 
-class Importer:
+class ImportTool:
     def __init__(self, node, service, git_repo_path='/home/ubuntu/data-import-containers'):
         """
         Constructor. Builds a base class for the import service (for both ELK and Prometheus)
@@ -129,19 +129,19 @@ class Importer:
 
     def stop_docker(self):
         try:
-            self.node.execute('sudo docker-compose down')
+            self.node.execute(f'sudo {self.repo_path}/{self.service}/docker-compose down')
         except Exception as e:
             print(f"Fail: {e}")
 
 
-class ElkExporter(MFLib, ElkViewer):
-    def __init__(self, slice_name, node_name="meas-node", local_storage_directory="/mflib"):
-        MFLib.__init__(self, slice_name, local_storage_directory)
+class ElkExporter(MFLib):
+    def __init__(self, slice_name="", local_storage_directory="/tmp/mflib", node_name="meas-node"):
+        super().__init__(slice_name, local_storage_directory)
         try:
             self.node = self.slice.get_node(name=node_name)
         except Exception as e:
+            print("Yeah")
             print(f"Fail: {e}")
-        ElkViewer.__init__(self, self.node)
         
 
     def create_repository(self, repository_name):
@@ -185,17 +185,6 @@ class ElkExporter(MFLib, ElkViewer):
                 print(f"Fail: {e}")
         self.view_snapshot_directory()
 
-    def view_snapshot_directory(self):
-        commands = [
-            'echo snapshots in directory on measurement node:',
-            'ls /home/mfuser/services/elk/files/snapshots/'
-        ]
-        for command in commands:
-            try:
-                self.node.execute(command)
-            except Exception as e:
-                print(f"Fail: {e}")
-
     def _ensure_dir_permissions(self):
         commands = [
             'sudo chown -R 1000:1000 /var/lib/docker/volumes/elk_snapshotbackup',
@@ -207,9 +196,56 @@ class ElkExporter(MFLib, ElkViewer):
             except Exception as e:
                 print(f"Fail: {e}")
 
+    ##### Query ELK data #####
 
-class PrometheusExporter(MFLib, ):
-    def __init__(self, slice_name, node_name="meas-node", local_storage_directory="/mflib"):
+    # View available indices
+    def view_indices(self):
+        """
+        show existing elk indices using elk rest api
+        
+        """
+        cmd = f'curl "http://localhost:9200/_cat/indices?v"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+    # View a repository
+    def view_repository(self, repository_name):
+        """
+        show existing elk repository using elk rest api
+        
+        """
+        cmd = f'curl -X GET "http://localhost:9200/_cat/snapshots/{repository_name}?pretty"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+    def view_snapshot(self, repository_name, snapshot_name):
+        """
+        show elk snapshot inside repository using elk rest api
+        """
+        cmd = f'curl -X GET "http://localhost:9200/_snapshot/{repository_name}/{snapshot_name}?pretty"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+    def view_snapshot_directory(self):
+        commands = [
+            'echo snapshots in directory on measurement node:',
+            'ls /home/mfuser/services/elk/files/snapshots/'
+        ]
+        for command in commands:
+            try:
+                self.node.execute(command)
+            except Exception as e:
+                print(f"Fail: {e}")
+
+
+class PrometheusExporter(MFLib):
+    def __init__(self, slice_name, local_storage_directory="/tmp/mflib", node_name="meas-node"):
         super().__init__(slice_name, local_storage_directory)
         try:
             self.node = self.slice.get_node(name=node_name)
@@ -248,7 +284,7 @@ class PrometheusExporter(MFLib, ):
                 print(f"Fail: {e}")
 
 
-class ElkImportTool(ElkViewer, Importer):
+class ElkImporter(ImportTool):
     def __init__(self, slice_name, node_name, git_repo_path='/home/ubuntu/data-import-containers'):
         self.slice_name = slice_name
         try:
@@ -259,10 +295,9 @@ class ElkImportTool(ElkViewer, Importer):
             self.node = self.slice.get_node(name=node_name)
         except Exception as e:
             print(f"Fail: {e}")
-        
-        ElkViewer.__init__(self, self.node)
-        Importer.__init__(self, self.node, "elk", git_repo_path)
+        super().__init__(self.node, "elk", git_repo_path)
 
+# TODO: Reminder - this only works for a snapshot in the snapshots/ directory on meas_node
     def import_snapshot(self, snapshot_file_name):
         """
         Untars snapshot file, creates repository directory and places snapshot data inside.
@@ -279,11 +314,11 @@ class ElkImportTool(ElkViewer, Importer):
 
         commands = [
             "echo 'Creating imported_data directory..'",
-            f"sudo mkdir {self.repo_path}/imported_data",
+            f"sudo mkdir {self.repo_path}/{self.service}//imported_data",
             "echo 'Moving snapshot file..'",
-            f"sudo mv /tmp/{snapshot_file_name} {self.repo_path}/snapshots/",
+            f"sudo mv /tmp/{snapshot_file_name} {self.repo_path}/{self.service}/snapshots/",
             "echo 'Untarring snapshot data into shared docker volume..'",
-            f"sudo tar -xvf {self.repo_path}/snapshots/{snapshot_file_name} -C {self.repo_path}/imported_data"
+            f"sudo tar -xvf {self.repo_path}/{self.service}/snapshots/{snapshot_file_name} -C {self.repo_path}/{self.service}/imported_data"
         ]
         for command in commands:
             try:
@@ -315,7 +350,7 @@ class ElkImportTool(ElkViewer, Importer):
 
     def remove_data(self):
         commands = [
-            'sudo docker volume rm elk-docker-container_es-data',
+            'sudo docker volume rm elk_es-data',
             f'rm -rf {self.repo_path}/imported_data/*'
         ]
         try:
@@ -324,8 +359,83 @@ class ElkImportTool(ElkViewer, Importer):
         except Exception as e:
             print(f"Fail: {e}")
 
+    ##### Query ELK data #####
 
-# class PrometheusImportTool(Importer):
-#     def __init__(self, slice_name, node_name, git_repo_path='/home/ubuntu/data-import-containers'):
-#         self.service = "prometheus"
-#         super().__init__(slice_name, node_name, self.service, git_repo_path)
+    # View available indices
+    def view_indices(self):
+        """
+        show existing elk indices using elk rest api
+        
+        """
+        cmd = f'curl "http://localhost:9200/_cat/indices?v"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+    # View a repository
+    def view_repository(self, repository_name):
+        """
+        show existing elk repository using elk rest api
+        
+        """
+        cmd = f'curl -X GET "http://localhost:9200/_cat/snapshots/{repository_name}?pretty"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+    def view_snapshot(self, repository_name, snapshot_name):
+        """
+        show elk snapshot inside repository using elk rest api
+        """
+        cmd = f'curl -X GET "http://localhost:9200/_snapshot/{repository_name}/{snapshot_name}?pretty"'
+        try:
+            self.node.execute(cmd)
+        except Exception as e:
+            print(f"Fail: {e}")
+
+
+class PrometheusImporter(ImportTool):
+    def __init__(self, slice_name, node_name, git_repo_path='/home/ubuntu/data-import-containers'):
+        self.slice_name = slice_name
+        try:
+            self.slice = fablib.get_slice(name=self.slice_name)
+        except Exception as e:
+            print(f"Fail: {e}")
+        try:
+            self.node = self.slice.get_node(name=node_name)
+        except Exception as e:
+            print(f"Fail: {e}")
+        super().__init__(self.node, "prometheus", git_repo_path)
+
+# TODO: Need an easy way to get snapshot file onto fabric slice if it is large
+    
+# TODO: 2.1 is wrong on the prometheus directions.
+    def import_snapshot(self, snapshot_file_name):
+        """
+        Uploads snapshot file into correct place on meas_node then runs import_snapshot.sh to import snapshot.
+        Args:
+        snapshot_file_name(str): ELK snapshot file name
+        snapshot_file_path(optional, str): ELK Snapshot file path on node (Defaults to snapshot)
+        repository_directory(optional, str): ELK Repository directory path on node (Defaults to git repo settings)
+        """
+        try:
+            self.node.execute(f"echo uploading {snapshot_file_name} to measurement node..")
+            self.node.upload_file(f"./snapshots/{snapshot_file_name}", f'/tmp/{snapshot_file_name}')
+        except Exception as e:
+            print(f"Fail: {e}")
+
+        commands = [
+            "echo 'Creating imported_data directory..'",
+            f"sudo mkdir {self.repo_path}/{self.service}/imported_data",
+            "echo 'Moving snapshot file..'",
+            f"sudo mv /tmp/{snapshot_file_name} {self.repo_path}/{self.service}/snapshots/",
+            "echo 'Untarring snapshot data into shared docker volume..'",
+            f"sudo tar -xvf {self.repo_path}/{self.service}/snapshots/{snapshot_file_name} -C {self.repo_path}/{self.service}/imported_data"
+        ]
+        for command in commands:
+            try:
+                self.node.execute(command)
+            except Exception as e:
+                print(f"Fail: {e}")
