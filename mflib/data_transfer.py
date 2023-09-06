f4a419dd2e27ef0f84df0548ac78ecc8d75db18f
jackhancockuky <jcha265@uky.edu>
2023-09-05 22:25:37 -0400

    Prometheus export/import are now both working fully

--- mflib/data_transfer.py
+++ mflib/data_transfer.py
@@ -26,53 +26,6 @@ import json
 from mflib.mflib import MFLib
 
 
-class ElkViewer:
-    def __init__(self, node):
-        """
-        Constructor. Builds a base class for functions to query ELK information
-        - Elk
-            - Export
-            - Import
-        """
-        self.node = node
-
-    ##### Query ELK data #####
-
-    # View available indices
-    def view_indices(self):
-        """
-        show existing elk indices using elk rest api
-        
-        """
-        cmd = f'curl "http://localhost:9200/_cat/indices?v"'
-        try:
-            self.node.execute(cmd)
-        except Exception as e:
-            print(f"Fail: {e}")
-
-    # View a repository
-    def view_repository(self, repository_name):
-        """
-        show existing elk repository using elk rest api
-        
-        """
-        cmd = f'curl -X GET "http://localhost:9200/_cat/snapshots/{repository_name}?pretty"'
-        try:
-            self.node.execute(cmd)
-        except Exception as e:
-            print(f"Fail: {e}")
-
-    def view_snapshot(self, repository_name, snapshot_name):
-        """
-        show elk snapshot inside repository using elk rest api
-        """
-        cmd = f'curl -X GET "http://localhost:9200/_snapshot/{repository_name}/{snapshot_name}?pretty"'
-        try:
-            self.node.execute(cmd)
-        except Exception as e:
-            print(f"Fail: {e}")
-
-
 class ImportTool:
     def __init__(self, node, service, git_repo_path='/home/ubuntu/data-import-containers'):
         """
@@ -297,7 +250,6 @@ class ElkImporter(ImportTool):
             print(f"Fail: {e}")
         super().__init__(self.node, "elk", git_repo_path)
 
-# TODO: Reminder - this only works for a snapshot in the snapshots/ directory on meas_node
     def import_snapshot(self, snapshot_file_name):
         """
         Untars snapshot file, creates repository directory and places snapshot data inside.
@@ -409,30 +361,15 @@ class PrometheusImporter(ImportTool):
             print(f"Fail: {e}")
         super().__init__(self.node, "prometheus", git_repo_path)
 
-# TODO: Need an easy way to get snapshot file onto fabric slice if it is large
-    
-# TODO: 2.1 is wrong on the prometheus directions.
     def import_snapshot(self, snapshot_file_name):
         """
         Uploads snapshot file into correct place on meas_node then runs import_snapshot.sh to import snapshot.
         Args:
-        snapshot_file_name(str): ELK snapshot file name
-        snapshot_file_path(optional, str): ELK Snapshot file path on node (Defaults to snapshot)
-        repository_directory(optional, str): ELK Repository directory path on node (Defaults to git repo settings)
+        snapshot_file_name(str): Prometheus snapshot file name
         """
-        try:
-            self.node.execute(f"echo uploading {snapshot_file_name} to measurement node..")
-            self.node.upload_file(f"./snapshots/{snapshot_file_name}", f'/tmp/{snapshot_file_name}')
-        except Exception as e:
-            print(f"Fail: {e}")
-
         commands = [
-            "echo 'Creating imported_data directory..'",
-            f"sudo mkdir {self.repo_path}/{self.service}/imported_data",
-            "echo 'Moving snapshot file..'",
-            f"sudo mv /tmp/{snapshot_file_name} {self.repo_path}/{self.service}/snapshots/",
-            "echo 'Untarring snapshot data into shared docker volume..'",
-            f"sudo tar -xvf {self.repo_path}/{self.service}/snapshots/{snapshot_file_name} -C {self.repo_path}/{self.service}/imported_data"
+            "echo 'Importing snapshot into Prometheus..'",
+            f"sudo {self.repo_path}/{self.service}/import_snapshot.sh /tmp/{snapshot_file_name} {self.repo_path}/{self.service}/snapshots"
         ]
         for command in commands:
             try:
